60cfe396f7c44e98f0974e29f0c72892232765cd
PC-DHARMO\Dharmo <id.dharmo@gmail.com>
2019-10-05 15:09:54 +0700

    Formating with json

--- getfact.py
+++ getfact.py
@@ -1,10 +1,10 @@
 from napalm import get_network_driver
-
+import json
 driver = get_network_driver("ios")
 ios_r1 = driver("10.10.10.1","dharmo","dharmo")
 ios_r1.open()
 
 r1_fact = ios_r1.get_facts()
-print r1_fact
+print json.dumps(r1_fact, indent=4)
 
 ios_r1.close()
--- getip.py
+++ getip.py
@@ -1,10 +1,10 @@
 from napalm import get_network_driver
-
+import json
 driver = get_network_driver("ios")
 ios_r1 = driver("10.10.10.1","dharmo","dharmo")
 ios_r1.open()
 
 r1_ip = ios_r1.get_interfaces_ip()
-print r1_ip
+print json.dumps(r1_ip, indent=4)
 
 ios_r1.close()
